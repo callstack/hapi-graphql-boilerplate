3cf3090ddc065d4c18ff9be0db75fc803e215321
Mike Grabowski <grabbou@gmail.com>
2016-10-17 13:53:12 +0200

    Add some comments

--- internals/modules/index.js
+++ internals/modules/index.js
@@ -1,3 +1,12 @@
+/**
+ * An array of all internal modules used by this Hapi.js instance.
+ *
+ * This is default configuration of the boilerplate. If you intend to use
+ * a different set of plugins, import and add to your manifest each of these
+ * separately.
+ *
+ * @flow
+ */
 import reporter from './reporter';
 
 export default [
--- internals/modules/reporter.js
+++ internals/modules/reporter.js
@@ -1,3 +1,32 @@
+/**
+ * @flow
+ */
+
+/**
+ * Reporter is essentialy an array of transforms. Each item in the array of streams will
+ * be piped together in the array order.
+ *
+ * The console reporter will:
+ * 1) Pick `error` events
+ * 2) Return server event into string with `good-console`
+ * 3) Pipe it directly to `stdout`
+ */
+const consoleReporter = [
+  {
+    module: 'good-squeeze',
+    name: 'Squeeze',
+    args: [
+      {
+        error: '*',
+      },
+    ],
+  },
+  {
+    module: 'good-console',
+  },
+  'stdout',
+];
+
 export default {
   plugin: {
     register: 'good',
@@ -6,21 +35,7 @@ export default {
         interval: 60000,
       },
       reporters: {
-        console: [
-          {
-            module: 'good-squeeze',
-            name: 'Squeeze',
-            args: [
-              {
-                error: '*',
-              },
-            ],
-          },
-          {
-            module: 'good-console',
-          },
-          'stdout',
-        ],
+        console: consoleReporter,
       },
     },
   },
