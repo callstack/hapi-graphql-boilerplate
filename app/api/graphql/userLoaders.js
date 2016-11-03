881227899e476335db346566c82eb5e4cdef7a6e
Jakub Stasiak <kubafr2@gmail.com>
2016-11-03 14:35:58 +0100

    Changed global dataloader to dataloader per request

---
+++ app/api/graphql/userLoaders.js
@@ -0,0 +1,20 @@
+/**
+ * @flow
+ */
+
+import Dataloader from 'dataloader';
+
+const getDataLoader =
+  (fieldName: string, schema: Object): Function =>
+    new Dataloader(async (ids: Array<string>): any => {
+      const results = await schema.find({ [fieldName]: { $in: ids } });
+      const resultMap = results.reduce((acc, r) => ({ ...acc, [r[fieldName]]: r }), {});
+
+      return ids.map((id) => resultMap[id]);
+    });
+
+export default (db) => ({
+  userById: getDataLoader('_id', db.User),
+  userByEmail: getDataLoader('email', db.User),
+});
+
